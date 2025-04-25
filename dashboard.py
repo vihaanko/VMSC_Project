5b9c038b5bd6b98d0cc85559414c50ee222e35ce
vihaanko <31429547+vihaanko@users.noreply.github.com>
2025-04-25 12:54:55 -0400

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -191,6 +191,8 @@ with tabs[3]:
     # Recommended Event Strategy Section
     st.header("Recommendation Engine")
     st.subheader("Recommended Event Strategy:")
+    df = cleaned_event_attendance.copy()
+    df['day_of_week'] = df['event_date'].dt.dayofweek
     newDF = recommend_schedule(cleaned_event_attendance)
     st.markdown(f"Best Day of the Week: {newDF['best_day']}")
     st.markdown(f"Best Month: {newDF['best_month']}")
